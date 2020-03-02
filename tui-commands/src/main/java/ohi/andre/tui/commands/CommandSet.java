910564d602b1a2bb6ff0e678b8756f5988a5194f
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-03-03 00:51:40 +0100

    CommandSet is no more a Singleton

--- tui-commands/src/main/java/ohi/andre/tui/commands/CommandSet.java
+++ tui-commands/src/main/java/ohi/andre/tui/commands/CommandSet.java
@@ -1,18 +1,34 @@
 package ohi.andre.tui.commands;
 
+import android.content.Context;
+
 import java.util.HashMap;
+import java.util.Iterator;
+
+import ohi.andre.tui.commands.raw.flash;
 
 public class CommandSet {
-    private static HashMap<String, AbstractCommand> commandSet;
+    private HashMap<String, AbstractCommand> commandSet;
 
-    private static void initialize() {
+    public CommandSet() {
         commandSet = new HashMap<>();
 
         // fill the set
+        add(new flash());
     }
 
-    public static AbstractCommand get(String commandName) {
-        initialize();
+    private void add(AbstractCommand command) {
+        commandSet.put(command.name(), command);
+    }
+
+    public AbstractCommand get(String commandName) {
         return commandSet.get(commandName);
+    }
+
+    public void dispose(Context context) {
+        Iterator<AbstractCommand> iterator = commandSet.values().iterator();
+        while (iterator.hasNext()) {
+            iterator.next().dispose(context);
+        }
     }
 }
