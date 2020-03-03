0c81a804ac896ada5f7203df1f9fc5f8089a4bf6
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-03-03 10:14:44 +0100

    first commit

--- tui-bridge/src/androidTest/java/ohi/andre/tui/bridge/ExampleInstrumentedTest.java
+++ tui-bridge/src/androidTest/java/ohi/andre/tui/bridge/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
-package ohi.andre.tui.core;
+package ohi.andre.tui.bridge;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ohi.andre.tui.platform.test", appContext.getPackageName());
+        assertEquals("ohi.andre.tui.bridge.test", appContext.getPackageName());
     }
 }
