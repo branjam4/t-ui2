dcb587b0f197779b1122cf4e8a51f3b0f8bf7e48
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-03-03 11:10:18 +0100

    new method parameters. exec gets an array of Parameter

--- tui-commands/src/main/java/ohi/andre/tui/commands/AbstractCommand.java
+++ tui-commands/src/main/java/ohi/andre/tui/commands/AbstractCommand.java
@@ -2,8 +2,11 @@ package ohi.andre.tui.commands;
 
 import android.content.Context;
 
+import ohi.andre.tui.commands.parameters.Parameter;
+
 public interface AbstractCommand {
     String name();
-    String exec(Context context);
+    String exec(Context context, Parameter[] parameters);
     void dispose(Context context);
+    Parameter[] parameters();
 }
