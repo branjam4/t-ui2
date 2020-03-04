7e0e068c02a166428333758f5de1bca876834d84
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-03-04 10:51:34 +0100

    add mockito to tui-core test

--- tui-core/src/test/java/ohi/andre/tui/core/CoreTest.java
+++ tui-core/src/test/java/ohi/andre/tui/core/CoreTest.java
@@ -1,16 +1,21 @@
 package ohi.andre.tui.core;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertNotNull;
 
 public class CoreTest {
     Core core;
+    @Mock
+    Context mockContext;
 
     @Before
     public void initialize() {
-        core = Core.getInstance(null);
+        core = Core.getInstance(mockContext);
     }
 
     @Test
