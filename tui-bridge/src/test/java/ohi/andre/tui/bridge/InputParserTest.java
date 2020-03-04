c2ffac221ddf72046858e3e966efa7372e218b16
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-03-04 11:34:58 +0100

    use mockito for testing

--- tui-bridge/src/test/java/ohi/andre/tui/bridge/InputParserTest.java
+++ tui-bridge/src/test/java/ohi/andre/tui/bridge/InputParserTest.java
@@ -2,21 +2,40 @@ package ohi.andre.tui.bridge;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import ohi.andre.tui.commands.CommandSet;
+import ohi.andre.tui.core.Core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class InputParserTest {
     InputParser parser;
     String s1, s2;
 
+    @Mock
+    Core mockCore;
+
+    @Mock
+    CommandSet mockSet;
+
     @Before
     public void prepareParser() {
+        when(mockCore.getCommandSet()).thenReturn(mockSet);
+
         parser = new InputParser();
 
         s1 = "test1";
         s2 = "test2";
 
-        parser.newInput(s1,s1);
-        parser.newInput(s2,s2);
+        parser.newInput(mockCore, s1,s1);
+        parser.newInput(mockCore, s2,s2);
     }
 
     @Test
